ae1309a12279a8e58f9d2328834764ed72227f94
aribandisrikanth-del <aribandi.srikanth@gmail.com>
2026-01-12 19:52:40 -0500

    Add files via upload

---
+++ age_checker.py
@@ -0,0 +1,8 @@
+age=int(input("type in your real age "))
+
+if age<10 and age>00 or age>20 and age>0:
+    print("you are under 10 or above 20")
+elif age<=0:
+    print("you have not been born or just been born")
+else:
+    print("you are 10 to 20 years old")
